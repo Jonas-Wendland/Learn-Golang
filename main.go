55a261c57f2d7ed9d0b78432e911b274ae28dfff
Jonas-Wendland <wendjon@gmx.de>
2025-05-13 09:41:16 +0200

    calculator add

--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"fmt"
 	"math/rand"
+
+	"github.com/Jonas-Wendland/Learn-Golang/calculator"
 )
 
 func main() {
 	fmt.Println("Hello, World!", rand.Intn(10))
+	fmt.Println("23 + 42 =", calculator.Add(23, 42))
 }
