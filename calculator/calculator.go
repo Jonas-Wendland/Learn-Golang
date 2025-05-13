5751272c051ff06990a2f388b80fb4adccffdeb3
Jonas-Wendland <wendjon@gmx.de>
2025-05-13 10:01:23 +0200

    types explanation

--- calculator/calculator.go
+++ calculator/calculator.go
@@ -1,7 +1,18 @@
 package calculator
 
+var Pi float64 = 3.141592654
+
 func Add(left int, right int) int {
-	return left + right
+	// var sum int = left + right
+
+	// var sum = left + right
+
+	// var sum int
+	// sum = left + right
+
+	sum := left + right
+
+	return sum
 }
 
 func Divide(left, right int) (quotient int, remainder int) {
